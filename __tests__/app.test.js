827c626b5f6b0718c914618ad226a5ff7166c31c
Gabriel Simek <gabrielsimek@outlook.com>
2021-06-29 21:48:44 -0700

    initial user post fail

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -7,5 +7,21 @@ describe('demo routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  
+  it('creates a user and signs them up', async () => {
+    const res = await request(app)
+      .post('/api/v1/auth/signup')
+      .send({
+        username: 'MrKitty',
+        profilePhotoUrl: 'http://placekitten.com/200/300',
+        password: 'password'
+      });
+
+    expect(res.body).toEqual({
+      id: '1',
+      username: 'MrKitty',
+      profilePhotoUrl: 'http://placekitten.com/200/300'
+    });
+
+
+  });
 });
