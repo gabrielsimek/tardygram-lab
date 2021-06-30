affe4b5b44016f4a217daf27759fc047f30167e5
Gabriel Simek <gabrielsimek@outlook.com>
2021-06-30 13:39:12 -0700

    get all posts test failling

--- __tests__/post.test.js
+++ __tests__/post.test.js
@@ -2,22 +2,22 @@ import pool from '../lib/utils/pool.js';
 import setup from '../data/setup.js';
 import request from 'supertest';
 import app from '../lib/app.js';
-//using before all!
 const agent = request.agent(app);
 describe('demo routes', () => {
-  beforeAll(() => {
-    return setup(pool);
-  });
-  //should hash password and authorize user
-  it('creates a post', async () => {
-      
-    const user = await agent
+  beforeEach(async()  => {
+    await setup(pool);
+    await agent
       .post('/api/v1/auth/signup')
       .send({
         username: 'MrKitty',
         profilePhotoUrl: 'http://placekitten.com/200/300',
         password: 'password'
       });
+  });
+  //should hash password and authorize user
+  it('creates a post', async () => {
+      
+  
 
     const res = await agent
       .post('/api/v1/posts')
@@ -36,6 +36,41 @@ describe('demo routes', () => {
       }
     );
   });
- 
+  it.only('gets a list of all posts', async () => {
+    const posts = [{
+      photoUrl: 'https://www.kitten.com/200/300',
+      caption: 'kitten',
+      tags: ['cat', 'cute']
+    }, 
+    {
+      photoUrl: 'https://www.placecage.com/g/200/300',
+      caption: 'cage',
+      tags: ['nick cage', 'calm']
+    },
+    {
+      photoUrl: 'https://www.placecage.com/c/200/300',
+      caption: 'cage',
+      tags: ['nick cage', 'crazy']
+    }
+    ];
 
+    const returnedPosts = await Promise.all(
+      posts.map(post => {
+        return agent
+          .post('/api/v1/posts')
+          .send(post);
+      })
+    );
+    
+    const res = await agent
+      .get('/api/v1/posts');
+
+    expect(res.body).toEqual(expect.arrayContaining(
+      [
+        returnedPosts[0].body,
+        returnedPosts[1].body,
+        returnedPosts[2].body
+      ]
+    ));
+  });
 });
