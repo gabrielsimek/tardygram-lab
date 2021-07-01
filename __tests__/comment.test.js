c00e103fa1b2a3cfd3585b5267ff0167f0c75e97
Gabriel Simek <gabrielsimek@outlook.com>
2021-06-30 18:07:27 -0700

    post comment test pass

--- __tests__/comment.test.js
+++ __tests__/comment.test.js
@@ -4,26 +4,21 @@ import request from 'supertest';
 import app from '../lib/app.js';
 const agent = request.agent(app);
 describe('demo routes', () => {
-  beforeEach(async()  => {
+  beforeAll(async()  => {
     await setup(pool);
-    await agent
-      .post('/api/v1/auth/signup')
-      .send({
-        username: 'MrKitty',
-        profilePhotoUrl: 'http://placekitten.com/200/300',
-        password: 'password'
-      });
   });
   it('creates a comment on a post', async () => {
-    const otherUser = await agent
+    //USER 1
+    await agent
       .post('/api/v1/auth/signup')
       .send({
         username: 'MrCat',
         profilePhotoUrl: 'http://placekitten.com/200/300',
-        password: 'strongPassword'
+        password: 'password'
       });
 
-    const post =  await agent
+    //post belongs to user w/ userId 1
+    await agent
       .post('/api/v1/posts')
       .send({
         photoUrl: 'https://www.placecage.com/200/300',
@@ -31,18 +26,47 @@ describe('demo routes', () => {
         tags: ['nick cage', 'national treasure']
       });
 
+
+    //USER 2
+    await agent
+      .post('/api/v1/auth/signup')
+      .send({
+        username: 'MsCat',
+        profilePhotoUrl: 'http://placekitten.com/200/300',
+        password: 'strongPassword'
+      });
+    //
+    //comment belongs to user w/ userId 2
+    
     const res = await agent
       .post('/api/v1/comments')
       .send({
         postId: '1',
         comment: 'Wow cool!'
       });
+
     expect(res.body).toEqual({
       id: '1',
-      userId: '1',
+      userId: '2',
       postId: '1',
       comment: 'Wow cool!'
     });
+  });
+  it.skip('deletes a comment', async () => {
+    //before all so previous comment is retained
+    //user2 currently logged in
+    const res = await agent
+      .delete('/api/v1/comments/1');
+
+
+    expect(res.body).toEqual(
+      {
+        id: '1',
+        userId: '2',
+        postId: '1',
+        comment: 'Wow cool!'
+      }
+    );
   });
 });
 
